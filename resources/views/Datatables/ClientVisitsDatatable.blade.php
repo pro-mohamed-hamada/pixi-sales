4e8a12084dfe916b1ea9f391a010a532609a81c0
pro-mohamed-hamada <pro.mohamed.hamada@gmail.com>
2024-01-27 21:45:00 +0200

    bug fix client visits

--- resources/views/Datatables/ClientVisitsDatatable.blade.php
+++ resources/views/Datatables/ClientVisitsDatatable.blade.php
@@ -20,10 +20,10 @@
                         @foreach ($client->visits as $visit)
                         <tr>
                             <td>{{ $visit->id }}</td>
-                            {{-- <td>{{ $visit->client->name }}</td>
+                            <td>{{ $visit->client->name }}</td>
                             <td>{{ $visit->action_type }}</td>
                             <td>{{ $visit->comment }}</td>
-                            <td>{{ $visit->created_at }}</td> --}}
+                            <td>{{ $visit->created_at }}</td>
                         </tr>
                         @endforeach 
                     </tbody>
