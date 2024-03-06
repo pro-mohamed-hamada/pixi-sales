83f1710d8042282f816ff6a676b63252dbf65d86
pro-mohamed-hamada <pro.mohamed.hamada@gmail.com>
2024-03-06 12:37:03 +0200

    fix auth resource

--- app/Http/Resources/AuthUserResource.php
+++ app/Http/Resources/AuthUserResource.php
@@ -26,35 +26,6 @@ class AuthUserResource extends JsonResource
                 "name"=> $this->name,
                 "email"=> $this->email,
             ],
-            "user_target"=>$this->whenLoaded("targets", UserTargetsResource::collection($this->targets)),
-            "personal_achievements"=>$this->whenLoaded("targets",
-            [
-                "visits"=>[
-                    "target_value" => $this->targets()->where('target', (string)TargetsEnum::VISIT)->sum('target_value'),
-                    "target_done" => $this->targets()->where('target', (string)TargetsEnum::VISIT)->sum('target_done'),
-                ],
-                "calls"=>[
-                    "target_value" => $this->targets()->where('target', (string)TargetsEnum::CALL)->sum('target_value'),
-                    "target_done" => $this->targets()->where('target', (string)TargetsEnum::CALL)->sum('target_done'),
-                ],
-                "meetings"=>[
-                    "target_value" => $this->targets()->where('target', (string)TargetsEnum::MEETING)->sum('target_value'),
-                    "target_done" => $this->targets()->where('target', (string)TargetsEnum::MEETING)->sum('target_done'),
-                ],
-                "proposals"=>[
-                    "target_value" => $this->targets()->where('target', (string)TargetsEnum::PROPOSAL)->sum('target_value'),
-                    "target_done" => $this->targets()->where('target', (string)TargetsEnum::PROPOSAL)->sum('target_done'),
-                ],
-                "clients"=>[
-                    "target_value" => $this->targets()->where('target', (string)TargetsEnum::CLIENT)->sum('target_value'),
-                    "target_done" => $this->targets()->where('target', (string)TargetsEnum::CLIENT)->sum('target_done'),
-                ],
-                "whatsapp_messages"=>[
-                    "target_value" => $this->targets()->where('target', (string)TargetsEnum::WHATSAPP_MESSAGE)->sum('target_value'),
-                    "target_done" => $this->targets()->where('target', (string)TargetsEnum::WHATSAPP_MESSAGE)->sum('target_done'),
-                ],
-            ]
-            , null),
        ];
     }
 
