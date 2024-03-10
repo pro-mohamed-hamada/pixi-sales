fe7e91209c62e7b20fa0029c3e3b23aa34b259c5
pro-mohamed-hamada <pro.mohamed.hamada@gmail.com>
2024-03-10 16:12:59 +0200

    bug fix tasks filters

--- app/QueryFilters/ClientServicesFilter.php
+++ app/QueryFilters/ClientServicesFilter.php
@@ -47,11 +47,11 @@ class ClientServicesFilter extends QueryFilter
         if (isset($term))
             return $this->builder->whereHas('client', function ($query) use ($term) {
                 $query->where('name', 'like', '%'.$term.'%')
-                ->where('phone', 'like', '%'.$term.'%')
-                ->where('company_name', 'like', '%'.$term.'%')
-                ->where('other_person_name', 'like', '%'.$term.'%')
-                ->where('other_person_phone', 'like', '%'.$term.'%')
-                ->where('other_person_position', 'like', '%'.$term.'%');
+                ->orWhere('phone', 'like', '%'.$term.'%')
+                ->orWhere('company_name', 'like', '%'.$term.'%')
+                ->orWhere('other_person_name', 'like', '%'.$term.'%')
+                ->orWhere('other_person_phone', 'like', '%'.$term.'%')
+                ->orWhere('other_person_position', 'like', '%'.$term.'%');
             });
     }
 
