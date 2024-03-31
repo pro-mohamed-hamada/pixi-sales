94d7f9ae68985eec376ba226cee0356ff26165ca
pro-mohamed-hamada <pro.mohamed.hamada@gmail.com>
2024-04-01 01:58:00 +0200

    clients filter

--- app/QueryFilters/ClientsFilter.php
+++ app/QueryFilters/ClientsFilter.php
@@ -3,6 +3,7 @@
 namespace App\QueryFilters;
 
 use App\Abstracts\QueryFilter;
+use Carbon\Carbon;
 
 class ClientsFilter extends QueryFilter
 {
@@ -12,11 +13,6 @@ class ClientsFilter extends QueryFilter
         parent::__construct($params);
     }
 
-    public function is_active($term)
-    {
-        return $this->builder->where('is_active',$term);
-    }
-
     public function phone($term)
     {
         return $this->builder->where('phone', $term);
@@ -24,7 +20,7 @@ class ClientsFilter extends QueryFilter
 
     public function other_person_phone($term)
     {
-        return $this->builder->where('phone', $term);
+        return $this->builder->where('other_person_phone', $term);
     }
 
     public function keyword($term)
@@ -51,6 +47,36 @@ class ClientsFilter extends QueryFilter
         });
     }
 
+    public function next_action($term)
+    {
+        return $this->builder->whereHas('visits', function ($query) use ($term) {
+            $query->where('next_action', $term)
+            ->whereDate('next_action_date', '>=', Carbon::now()->format('Y-m-d'));
+        })->orWhereHas('calls', function ($query) use ($term) {
+            $query->where('next_action', $term)
+            ->whereDate('next_action_date', '>=', Carbon::now()->format('Y-m-d'));
+        })->orWhereHas('meetings', function ($query) use ($term) {
+            $query->where('next_action', $term)
+            ->whereDate('next_action_date', '>=', Carbon::now()->format('Y-m-d'));
+        })->orWhereHas('services', function ($query) use ($term) {
+            $query->where('client_services.next_action', $term)
+            ->whereDate('next_action_date', '>=', Carbon::now()->format('Y-m-d'));
+        });
+    }
+
+    public function next_action_date($term)
+    {
+        return $this->builder->whereHas('visits', function ($query) use ($term) {
+            $query->whereDate('next_action_date', Carbon::parse($term)->format('Y-m-d'));
+        })->orWhereHas('calls', function ($query) use ($term) {
+            $query->whereDate('next_action_date', Carbon::parse($term)->format('Y-m-d'));
+        })->orWhereHas('meetings', function ($query) use ($term) {
+            $query->whereDate('next_action_date', Carbon::parse($term)->format('Y-m-d'));
+        })->orWhereHas('services', function ($query) use ($term) {
+            $query->whereDate('client_services.next_action_date', Carbon::parse($term)->format('Y-m-d'));
+        });
+    }
+
     public function city_id($term)
     {
         return $this->builder->where('city_id', $term);
@@ -68,7 +94,12 @@ class ClientsFilter extends QueryFilter
 
     public function created_at($term)
     {
-        return $this->builder->where('created_at', $term);
+        return $this->builder->whereDate('created_at', Carbon::parse($term)->format('Y-m-d'));
+    }
+
+    public function source_id($term)
+    {
+        return $this->builder->where('source_id', $term);
     }
 
 }
